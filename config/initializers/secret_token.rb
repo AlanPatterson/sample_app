a7b1ca6e4fecf672590f9f8d0d23fcba49954f54
Alan Patterson <alan.f.patterson@nasa.gov>
2012-05-18 11:11:58 -0500

    Initial Sample_app commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'c2560673361359779945fa2c39b40926cf07751645d836a6e1af78234b8f3a524d31d5a4e8956074ed10b99443628a60aa1aef5b9d55eee15618563e88b0b85c'
